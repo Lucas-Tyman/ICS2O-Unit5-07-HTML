eafbbca77a419249b053dcf5eedd1cec659164cb
Lucas Tyman <99354994+Lucas-Tyman@users.noreply.github.com>
2022-06-14 02:18:37 +0000

    4 files

--- js/script.js
+++ js/script.js
@@ -1,19 +1,35 @@
 // Created by: Lucas Tyman
-// Created on: Apr 2022
+// Created on: May 2022
 // This file contains the JS functions for index.html
+
+'use strict'
 
 /**
  * Check service worker.
  */
 if (navigator.serviceWorker) {
-  navigator.serviceWorker.register("/ICS2O-PWA-Template/sw.js", {
-    scope: "/ICS2O-PWA-Template/",
+  navigator.serviceWorker.register("/ICS2O-Unit5-07-HTML/sw.js", {
+    scope: "/ICS2O-Unit5-07-HTML/",
   })
 }
 
 /**
- * This function displays an alert.
+ * This function calculates using a for loop.
  */
-function myButtonClicked() {
-  document.getElementById("hello-world").innerHTML = "<p>Hello, World!</p>"
+function calculate() {
+  // input
+  var userInput = document.getElementById("user-input").value
+
+  // process and output
+  var answer = 0
+  var userInputAsInt = parseInt(userInput)
+  
+  if (userInputAsInt <= 0) {
+    document.getElementById("answer").innerHTML = "Please input a positive whole number!"
+  } else {
+    for (let counter = 1; counter <= userInputAsInt; counter++) {
+      answer += counter
+    }
+    document.getElementById("answer").innerHTML = "The value is " + (answer)
+  }
 }
